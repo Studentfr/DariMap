b77c31da6901bb8b3b2aeff7611a1f7449332501
Sungat Kaparov <sungatkaparov8@gmail.com>
2021-04-09 19:55:11 +0600

    Added: CRUD for Drug model, endpoints for CRUD in urls.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,5 +4,9 @@ from mainapp import views
 
 urlpatterns = [
     path('', views.index),
-    path('drug-list/', views.drugList),
+    path('drug-list/', views.drugList, name="drug-list"),
+    path('drug-detail/<int:pk>/', views.drugDetail, name="drug-detail"),
+    path('drug-update/<int:pk>/', views.drugUpdate, name="drug-update"),
+    path('drug-delete/<int:pk>/', views.drugDelete, name="drug-delete"),
+    path('drug-create/', views.drugCreate, name="drug-create"),
 ]
