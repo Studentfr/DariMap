2d3f74000792c2170436d63bb100af20405a72e3
Miras <sektor.miras@gmail.com>
2021-04-29 00:25:41 +0600

    added: raw sidebar design

--- frontend/src/Components/Sidebar/Sidebar.js
+++ frontend/src/Components/Sidebar/Sidebar.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import Drawer from "./Drawer";
+import SidebarContent from "./SidebarContent";
 
 function Sidebar({id, isHidden}) {
-
+    return(
+        <div className="overflow-hidden h-screen sticky top-0 flex">
+            <div className="absolute flex top-0 h-screen">
+                <Drawer/>
+                <SidebarContent/>
+            </div>
+        </div>
+    )
 }
 
 export default Sidebar;
---
+++ frontend/src/Components/Sidebar/SidebarContent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SidebarContent(props) {
+    return (
+        <div className="bg-yellow-200 overflow-hidden flex items-center justify-center">
+            <div className="w-48 text-center font-bold text-xl">Sidebar</div>
+        </div>
+    )
+}
+
+export default SidebarContent
