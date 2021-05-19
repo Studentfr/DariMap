bd58d2bffbbad5417b374779d0816e0471e4c092
joker <muratbekauyez>
2021-05-19 16:38:06 +0600

    Login and Register js forms

--- frontend/src/Components/Register/Register.js
+++ frontend/src/Components/Register/Register.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import "./Register.css"
 
 function Register() {
     return (
-        <div>
-            Register
+        <div className={"register"}>
+            <div className={"register-triangle"}/>
+
+            <h2 className={"register-header"}>Sign up</h2>
+
+            <form className={"register-container"}>
+                <p><input type={"username"} placeholder={"Username"}/></p>
+                <p><input type={"password"} placeholder={"Password"}/></p>
+                <p><input type={"password"} placeholder={"Re-Password"}/></p>
+                <p>
+                    <select name={"roles"} id={"roles"}>
+                        <option value={"client"}>Client</option>
+                        <option value={"pharmacy"}>Pharmacy</option>
+                    </select>
+                </p>
+                <p><input type={"submit"} value={"Register"}/></p>
+            </form>
         </div>
     )
 }
