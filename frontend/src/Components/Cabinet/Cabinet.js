5b9c5be490847d7aaf3a082fa9526cf7e4f40734
Miras <sektor.miras@gmail.com>
2021-05-26 02:05:40 +0600

    added: favourite pharmacy delete and show functionality

--- frontend/src/Components/Cabinet/Cabinet.js
+++ frontend/src/Components/Cabinet/Cabinet.js
@@ -1,12 +1,54 @@
 import "./Cabinet.css"
+import {useEffect, useState} from "react";
 
 function Cabinet() {
+    const [favPharmacies, setFavPharmacies] = useState(
+        [
+            {
+                id: 0,
+                pharmacy_id: {
+                    id: 0,
+                    name: "",
+                    address: "",
+                    phone_number: ""
+                }
+            }
+        ]
+    )
+
+    useEffect(() => {
+        fetch(`api/favourite-pharmacy-list/${localStorage.getItem('user_id')}`)
+            .then(response => response.json())
+            .then((pharmacies) => {
+                setFavPharmacies(pharmacies)
+                console.log(pharmacies)
+            },
+            (error) => {
+                console.log(error)
+            })
+    }, [])
+
+    const deleteFavPharmacy = (id) => (e) => {
+        e.preventDefault()
+        console.log(`DELETE PHARMACY WITH ID = ${id}`)
+        const requestOptions = {
+            method: 'DELETE'
+        }
+        fetch(`api/favourite-pharmacy-delete/${id}/`, requestOptions)
+            .then(response => response.json())
+            .then(() => {
+                setFavPharmacies(favPharmacies.filter((pharmacy) => pharmacy.id !== id))
+            },
+            (error) => {
+                console.log(error)
+            })
+    }
+
     return (
         <div className="limiter">
             <div className="container-table100">
                 <div className="wrap-table100">
                     <div className="table">
-
                         <div className="row header">
                             <div className="cell">
                                 Favourite Pharmacies
@@ -14,39 +56,21 @@ function Cabinet() {
                             <div className="cell">
                             </div>
                         </div>
+                        {favPharmacies.map((pharmacy) => {
+                            return (
+                                <div className="row" key={pharmacy.id}>
 
-
-                        <div className="row">
-
-                            <div className="cell" data-title="Pharmacies">
-                                Биосфера
-                            </div>
-                            <div className="cell" data-title="Delete">
-                                <button type="button" className="delete">Delete</button>
-                            </div>
-
-                        </div>
-
-                        <div className="row">
-
-                            <div className="cell" data-title="Pharmacies">
-                                Центральная аптека
-                            </div>
-                            <div className="cell" data-title="Delete">
-                                <button type="button" className="delete">Delete</button>
-                            </div>
-                        </div>
-
-                        <div className="row">
-                            <div className="cell" data-title="Pharmacies">
-                                Инфо+
-                            </div>
-                            <div className="cell" data-title="Delete">
-                                <button type="button" className="delete">Delete</button>
-                            </div>
-                        </div>
-
-
+                                    <div className="cell" data-title="Pharmacies">
+                                        <p>{pharmacy.pharmacy_id.name}</p>
+                                        <p>{pharmacy.pharmacy_id.address}</p>
+                                        <p>{pharmacy.pharmacy_id.phone_number}</p>
+                                    </div>
+                                    <div className="cell" data-title="Delete">
+                                        <button type="button" className="delete" onClick={deleteFavPharmacy(pharmacy.id)}>Delete</button>
+                                    </div>
+                                </div>
+                            )
+                        })}
                     </div>
                 </div>
             </div>
